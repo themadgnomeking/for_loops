07e3c4a55dcd98bff35704a98112e07aa72bbd03
Tims Guynes <tims.m.guynes@gmail.com>
2023-02-25 17:43:59 -0800

    added 4 examples of foreach

--- Program.cs
+++ Program.cs
@@ -32,3 +32,46 @@ for (i = 0, Console.WriteLine($"Start: i={i}, j={j}"); i < j; i++, j--, Console.
 
 
 //**************FOREACH***************
+//example 1
+/*
+var fibNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13};
+foreach (int element in fibNumbers)
+{
+    Console.Write($"{element} ");
+}
+*/
+
+//example 2
+/*
+Span<int> numbers = new int[] { 3, 14, 15, 92, 6};
+foreach (int number in numbers)
+{
+    Console.Write($"{number} ");
+}
+*/
+
+//example 3
+/*
+Span<int> storage = stackalloc int[10];
+int num = 0;
+foreach (ref int item in storage)
+{
+    item = num++;
+}
+foreach (ref readonly var item in storage)
+{
+    Console.Write($"{item} ");
+}
+*/
+
+//example 4 - awaite foreach
+/*
+//error error CS0103: The name 'GenerateSequenceAsync' does not exist in the current context
+//note for later to learn more
+await foreach (var item in GenerateSequenceAsync())
+{
+    Console.WriteLine(item);
+}
+*/
+
+//***************************DO/WHILE**************************
