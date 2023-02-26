2a5594c4f1469c3b64dd90ac0baa8381d071c32d
Tims Guynes <tims.m.guynes@gmail.com>
2023-02-25 17:25:44 -0800

    added 3 examples for loop

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
 ﻿// for and foreach loop practices
 // Examples
-
+/*
 for (int i = 0; i < 3; i++)
 {
     Console.Write(i);
 }
 // Output:
 // 012
+*/
+/*
+// increase by 2
+for (int i = 0; i < 30; i+=2)
+{
+    Console.Write(i + " ");
+}
+// Output:
+// 024681012...
+*/
+/*
+int i;
+int j = 3;
+for (i = 0, Console.WriteLine($"Start: i={i}, j={j}"); i < j; i++, j--, Console.WriteLine($"Step: i={i}, j={j}"))
+{
+    //...
+}
+// Output:
+// Start: i=0, j=3
+// Step: i=1, j=2
+// STep i=2, j=1
+*/
+
+
+//**************FOREACH***************
